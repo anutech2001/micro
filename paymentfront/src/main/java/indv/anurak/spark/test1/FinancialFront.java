82885e3847a190b5461aa91b73317a378197059c
vorawats-PC\vorawat.s <vorawats@gmail.com>
2016-12-21 10:11:56 +0700

    add argument environment paymentHost , paymentPort

--- paymentfront/src/main/java/indv/anurak/spark/test1/FinancialFront.java
+++ paymentfront/src/main/java/indv/anurak/spark/test1/FinancialFront.java
@@ -14,16 +14,26 @@ public class FinancialFront {
 	//private final static String QUEUE_NAME = "payment";
 	public static void main(final String[] args) {
 		
-		String frontPort = System.getenv("FrontPort");
+		String frontPort = System.getenv("FRONT_PORT");
+		String paymentHost = System.getenv("PAYMENT_HOST");
+		String paymentPort = System.getenv("PAYMENT_PORT");
+		
+		if(null == paymentHost){
+			paymentHost = "127.0.0.1";
+		}
+		if(null == paymentPort){
+			paymentPort = "8102";
+		}
+		String paymentUrl = "http://"+paymentHost+":"+paymentPort+"/"+"payment";
 		if(null != frontPort){
 			port(Integer.parseInt(frontPort));
 		}
         get("/payment", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             model.put("service", "Payment");
+            model.put("paymentUrl", paymentUrl);
             return new ModelAndView(model, "financialFrontPayment.vm");
         }, new VelocityTemplateEngine());
-
     }
 
 }
